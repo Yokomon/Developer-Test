101e98794415f35b3f3cfffebf8c3bce427c9045
Marow Macaulay <marowmars@gmail.com>
2019-05-21 01:37:13 +0100

    I created a stor component, a saga for our fetched API's and a reducer component for our images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-import React from "react";
-import axios from "axios";
+import React, { Component, Fragment } from "react";
+import { Provider } from "react-redux";
+import Header from "./Header";
+import ImageGrid from "./ImageGrid";
 
-class App extends React.Component {}
+import configureStore from "./store";
+
+const store = configureStore();
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <ImageGrid />
+        </Fragment>
+      </Provider>
+    );
+  }
+}
+
 export default App;
